7448172fa18e36cb3f31f55a2016bbff5b6f8054
Piumi Kavindya <piyumikavindyappk@gmail.com>
2025-06-23 15:55:46 +0530

    phq9 implemented to chat

---
+++ utils/phq9_questions.py
@@ -0,0 +1,11 @@
+PHQ9_QUESTIONS = [
+    { "id": 1, "question": "Little interest or pleasure in doing things" },
+    { "id": 2, "question": "Feeling down, depressed, or hopeless" },
+    { "id": 3, "question": "Trouble falling or staying asleep, or sleeping too much" },
+    { "id": 4, "question": "Feeling tired or having little energy" },
+    { "id": 5, "question": "Poor appetite or overeating" },
+    { "id": 6, "question": "Feeling bad about yourself — or that you are a failure or have let yourself or your family down" },
+    { "id": 7, "question": "Trouble concentrating on things, such as reading the newspaper or watching TV" },
+    { "id": 8, "question": "Moving or speaking slowly, or being fidgety or restless" },
+    { "id": 9, "question": "Thoughts that you would be better off dead or of hurting yourself" }
+]
