639f0e84c9a70072a1868ce8149c6d358d74e25d
Piumi Kavindya <piyumikavindyappk@gmail.com>
2025-08-18 00:55:57 +0530

    connect Mistral finetune model

--- utils/tts.py
+++ utils/tts.py
@@ -2,6 +2,8 @@ from gtts import gTTS
 import tempfile
 
 def generate_tts_audio(text: str) -> str:
+    if not text or not text.strip():
+        return None
     tts = gTTS(text, lang="en")
     temp_audio = tempfile.NamedTemporaryFile(delete=False, suffix=".mp3")
     tts.save(temp_audio.name)
